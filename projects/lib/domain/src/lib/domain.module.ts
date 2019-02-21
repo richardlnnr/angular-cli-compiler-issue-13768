ebb14d70bbe94d304367cae47829208ddba9cee5
Richard Lindner <richard.lnnr@gmail.com>
2019-02-21 15:14:14 -0300

    domain skeleton

---
+++ projects/lib/domain/src/lib/domain.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { DomainComponent } from './domain.component';
+
+@NgModule({
+  declarations: [DomainComponent],
+  imports: [
+  ],
+  exports: [DomainComponent]
+})
+export class DomainModule { }
